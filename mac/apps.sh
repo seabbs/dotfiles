4c6c1d95078d54606b7cb072d715d9a581b61588
Sam Abbott <contact@samabbott.co.uk>
2025-01-06 13:27:24 +0000

    Update apps.sh

--- mac/apps.sh
+++ mac/apps.sh
@@ -1,17 +1,11 @@
 #!bin/bash
 
 brew install --cask iterm2
-brew install --cask fantastical
 brew install --cask slack
 brew install --cask alfred
 brew install --cask rstudio
-brew install --cask visual-studio-code
+brew install --cask cursor
 brew install --cask github
-brew install --cask dropbox
 brew install --cask google-drive
 brew install --cask obsidian
-brew install --cask firefox
-brew install --cask 1password
-brew install --cask spotify
-brew install --cask zotero
 brew install --cask zoom
